e88837c505db6b470a8c48b2567ff8e025a5c759
Jaris <jaris@liminalvr.com>
2019-07-15 11:05:57 +1000

    Update preferences window and removed dependency on json

--- Editor/Scripts/PreferencesWindow.cs
+++ Editor/Scripts/PreferencesWindow.cs
@@ -1,122 +1,131 @@
 ﻿#if UNITY_EDITOR
-using System.IO;
-using Flow;
-using UnityEditor;
-using UnityEngine;
-//using Newtonsoft.Json;
-
-public class PreferencesWindow
-    : EditorWindow
+namespace Dekuple.Editor
 {
-    private static Texture2D _liminalLogo;
-    private const string _preferencesPath = "Preferences/Dekuple/Preferences.json";
-    private const string _preferencesDir = "Preferences/Dekuple/";
-    private bool _changes;
+    using System.IO;
+    using System.Diagnostics;
+    using System.Runtime.Serialization.Formatters.Binary;
 
-    [MenuItem("Liminal/Dekuple/Preferences")]
-    private static void Init()
+    using UnityEditor;
+    using UnityEngine;
+
+    using Flow;
+
+    using Debug = UnityEngine.Debug;
+
+    public class PreferencesWindow
+        : EditorWindow
     {
-        LoadPreferences();
+        private static Texture2D _liminalLogo;
+        private static string _preferencesPath => Path.Combine(_preferencesDir, "Preferences.dks");
+        private static string _preferencesDir => Path.Combine(Application.persistentDataPath, "Preferences/Dekuple/");
+        private static string _logoPath => "Packages/com.liminal.dekuple/Editor/Textures/LiminalLogo.png";
 
-        _liminalLogo = (Texture2D)AssetDatabase.LoadAssetAtPath("Packages/Dekuple/Editor/Textures/LiminalLogo.png", typeof(Texture2D));
-        var window = (PreferencesWindow)GetWindow(typeof(PreferencesWindow), true, "Liminal Preferences", true);
-        window.minSize = new Vector2(400, 200);
-        window.Show();
-    }
+        private bool _changes;
 
-    [InitializeOnLoadMethod]
-    private static void LoadPreferences()
-    {
-        if (!File.Exists(_preferencesPath))
-            return;
-
-        //Preferences.Prefs = JsonConvert.DeserializeObject<Preferences>(File.ReadAllText(_preferencesPath));
-    }
-
-    private void OnGUI()
-    {
-        EditorGUI.BeginChangeCheck();
-        if (Preferences.Prefs == null)
-            Preferences.Prefs = new Preferences();
-
-        DrawLogo();
-        DrawHeading();
-
-        GUILayout.Space(6);
-
-        Preferences.Prefs.UseViewBaseInspector = GUILayout.Toggle(Preferences.Prefs.UseViewBaseInspector, "Use Custom View Inspector");
-        Preferences.Prefs.UseViewBaseHierarchy = GUILayout.Toggle(Preferences.Prefs.UseViewBaseHierarchy, "Enable Hierarchy Labels");
-
-        GUILayout.Space(2);
-        GUILayout.Label("Logging Level");
-        Preferences.Prefs.LogLevel = (ELogLevel)GUILayout.Toolbar((int)Preferences.Prefs.LogLevel, typeof(ELogLevel).GetEnumNames());
-
-        GUILayout.FlexibleSpace();
-        GUILayout.BeginHorizontal();
-        GUILayout.FlexibleSpace();
-
-        if (EditorGUI.EndChangeCheck())
-            _changes = true;
-
-        if (GUILayout.Button("Apply", GUILayout.MinWidth(100), GUILayout.Height(22)))
+        [MenuItem("Liminal/Dekuple/Preferences")]
+        private static void Init()
         {
-            SaveChanges();
-            _changes = false;
+            Load();
+
+            _liminalLogo = (Texture2D)AssetDatabase.LoadAssetAtPath(_logoPath, typeof(Texture2D));
+            var window = (PreferencesWindow)GetWindow(typeof(PreferencesWindow), true, "Liminal Preferences", true);
+            window.minSize = new Vector2(400, 200);
+            window.Show();
         }
 
-        if (_changes)
+        private static void Save()
         {
-            var rect = GUILayoutUtility.GetLastRect();
-            GUI.Label(rect, GUIContent.none, "LightmapEditorSelectedHighlight");
+            if (!Directory.Exists(_preferencesDir))
+                Directory.CreateDirectory(_preferencesDir);
+
+            BinaryFormatter formatter = new BinaryFormatter();
+            FileStream fs = File.Create(_preferencesPath);
+            formatter.Serialize(fs, Preferences.Prefs);
+            fs.Close();
+            Debug.Log("Updated preferences for Dekuple.");
         }
 
-        GUILayout.Space(2);
-        GUILayout.EndHorizontal();
-        GUILayout.Space(6);
-    }
+        [InitializeOnLoadMethod]
+        private static void Load()
+        {
+            if (!File.Exists(_preferencesPath))
+                return;
 
-    private static void DrawHeading()
-    {
-        GUILayout.BeginHorizontal();
-        GUILayout.FlexibleSpace();
-        GUILayout.Label("Dekuple Preferences", new GUIStyle("AM MixerHeader") { alignment = TextAnchor.MiddleCenter });
-        GUILayout.FlexibleSpace();
-        GUILayout.EndHorizontal();
-    }
+            BinaryFormatter formatter = new BinaryFormatter();
+            FileStream fs = File.Open(_preferencesPath, FileMode.Open);
+            Preferences.Prefs = (Preferences)formatter.Deserialize(fs);
+            fs.Close();
+        }
 
-    private static void SaveChanges()
-    {
-        if (!Directory.Exists(_preferencesDir))
-            Directory.CreateDirectory(_preferencesDir);
+        private void OnGUI()
+        {
+            EditorGUI.BeginChangeCheck();
+            if (Preferences.Prefs == null)
+                Preferences.Prefs = new Preferences();
 
-        //var jsonString = JsonConvert.SerializeObject(Preferences.Prefs, Formatting.Indented);
-        //File.WriteAllText(_preferencesPath, jsonString);
-        Debug.Log("Updated preferences for Dekuple.");
-    }
+            DrawLogo();
+            DrawHeading();
 
-    private static void DrawLogo()
-    {
-        if (_liminalLogo != null)
+            GUILayout.Space(6);
+
+            Preferences.Prefs.UseViewBaseInspector =
+                GUILayout.Toggle(Preferences.Prefs.UseViewBaseInspector, "Use Custom View Inspector");
+            Preferences.Prefs.UseViewBaseHierarchy =
+                GUILayout.Toggle(Preferences.Prefs.UseViewBaseHierarchy, "Enable Hierarchy Labels");
+
+            GUILayout.Space(2);
+            GUILayout.Label("Logging Level");
+            Preferences.Prefs.LogLevel = GUILayout.Toolbar(Preferences.Prefs.LogLevel, typeof(ELogLevel).GetEnumNames());
+            Preferences.Prefs.Verbosity = EditorGUILayout.IntSlider("Verbosity", Preferences.Prefs.Verbosity, 0, 100);
+
+            GUILayout.FlexibleSpace();
+            GUILayout.BeginHorizontal();
+            GUILayout.FlexibleSpace();
+
+            if (EditorGUI.EndChangeCheck())
+                _changes = true;
+
+            if (GUILayout.Button("Apply", GUILayout.MinWidth(100), GUILayout.Height(22)))
+            {
+                Save();
+                _changes = false;
+            }
+
+            if (_changes)
+            {
+                var rect = GUILayoutUtility.GetLastRect();
+                GUI.Label(rect, GUIContent.none, "LightmapEditorSelectedHighlight");
+            }
+
+            GUILayout.Space(2);
+            GUILayout.EndHorizontal();
+            GUILayout.Space(6);
+        }
+
+        private static void DrawHeading()
         {
             GUILayout.BeginHorizontal();
             GUILayout.FlexibleSpace();
-            GUILayout.Label(_liminalLogo, GUILayout.Width(200), GUILayout.Height(44));
+            GUILayout.Label("Dekuple Preferences", new GUIStyle("AM MixerHeader") { alignment = TextAnchor.MiddleCenter });
             GUILayout.FlexibleSpace();
             GUILayout.EndHorizontal();
         }
-        else
+
+        private static void DrawLogo()
         {
-            GUILayout.Space(4);
+            if (_liminalLogo != null)
+            {
+                GUILayout.BeginHorizontal();
+                GUILayout.FlexibleSpace();
+                GUILayout.Label(_liminalLogo, GUILayout.Width(200), GUILayout.Height(44));
+                GUILayout.FlexibleSpace();
+                GUILayout.EndHorizontal();
+            }
+            else
+            {
+                GUILayout.Space(4);
+            }
         }
     }
-}
-
-public class Preferences
-{
-    public static Preferences Prefs;
-
-    public bool UseViewBaseInspector;
-    public ELogLevel LogLevel;
-    public bool UseViewBaseHierarchy;
 }
 #endif
